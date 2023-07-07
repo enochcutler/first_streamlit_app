1d0592b193962800c3b883ce248f347263df92f9
enochcutler <137871530+enochcutler@users.noreply.github.com>
2023-07-07 12:10:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-south-1.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
