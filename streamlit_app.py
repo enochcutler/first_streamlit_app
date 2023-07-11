4ddc2f44ae1fe1ab61a79deb89502b79519adcb3
enochcutler <137871530+enochcutler@users.noreply.github.com>
2023-07-11 16:20:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
 
 #Create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice): 
