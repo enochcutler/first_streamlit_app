de301144cd600d18e445ef3fc07a8cdabca6a77a
enochcutler <137871530+enochcutler@users.noreply.github.com>
2023-07-11 11:55:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,19 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+     streamlit.error("Please select a fruit to get information.")
+     #streamlit.write('The user entered ', fruit_choice)
+   else:
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+     # write your own comment -what does the next line do? 
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     # write your own comment - what does this do?
+     streamlit.dataframe(fruityvice_normalized)
+except URL Error as e:
+  streamlit.error()
 # stopping temporarily
 streamlit.stop()
 
